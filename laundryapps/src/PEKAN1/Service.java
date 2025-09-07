b95f9d6b1dff032c122eadaac89106a26d28246b
iMaIrsdyh <karimahirsyadiyah@gmail.com>
2025-09-07 23:44:11 +0700

    PRATIKUM PEKAN 1 PBO

---
+++ laundryapps/src/PEKAN1/Service.java
@@ -0,0 +1,25 @@
+package PEKAN1;
+
+//Karimah Irsyadiyah
+//2411533018
+
+public class Service {
+    private String id;
+    private String jenis;
+    private double harga;
+    private String status;
+
+    public Service() {}
+
+    public String getId() { return id; }
+    public void setId(String id) { this.id = id; }
+
+    public String getJenis() { return jenis; }
+    public void setJenis(String jenis) { this.jenis = jenis; }
+
+    public double getHarga() { return harga; }
+    public void setHarga(double harga) { this.harga = harga; }
+
+    public String getStatus() { return status; }
+    public void setStatus(String status) { this.status = status; }
+}
